03f35cc43c7ba270a7126f84f576206894de7e1c
thesebas <thesebas@thesebas.net>
2024-05-21 23:55:38 +0200

    add missing new-lines

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import ini from 'ini'
 
-export default function (content) {
+/** function to convert INI file contents to js required by webpack loader */
+export default function (content: string): string {
 	this.cacheable && this.cacheable();
 	return 'module.exports = ' + JSON.stringify(ini.parse(content)) + ';';
 };
